f0519dfcd34cd752e7086fca9f336d873fcf7547
bralbral <bbralyc@gmail.com>
2024-06-22 09:20:10 +0300

    fix

--- helm-release.bash
+++ helm-release.bash
@@ -1,31 +1,47 @@
 #!/bin/bash
 
-# Путь к исходным чартам
-SOURCE_DIR="./helm/sources"
+# Параметры скрипта
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--repository-id) ARTIFACTHUB_REPO_ID="$2"; shift ;;
+        -s|--source-dir) SOURCE_DIR="$2"; shift ;;
+        -d|--releases-dir) RELEASES_DIR="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
-# Путь к релизам
-RELEASES_DIR="./helm/releases"
+# Проверка наличия обязательных параметров
+if [[ -z "$ARTIFACTHUB_REPO_ID" || -z "$SOURCE_DIR" || -z "$RELEASES_DIR" ]]; then
+    echo "Usage: $0 -r|--repository-id <artifacthub-repo-id> -s|--source-dir <source-directory> -d|--releases-dir <releases-directory>"
+    exit 1
+fi
 
 # URL для хранилища Helm
 BASE_URL="https://bralbral.github.io/helm/releases"
 
-# Упаковка и обновление индекса для каждого чарт-проекта
-for dir in $SOURCE_DIR/*; do
-  if [ -d "$dir" ]; then
-    # Получение имени папки проекта
-    project_name=$(basename "$dir")
+# Получение имени папки проекта
+project_name=$(basename "$SOURCE_DIR")
 
-    # Путь к папке релизов проекта
-    target_dir="$RELEASES_DIR/$project_name"
+# Путь к папке релизов проекта
+target_dir="$RELEASES_DIR/$project_name"
 
-    # Очистка целевой папки
-    rm -rf "$target_dir"
-    mkdir -p "$target_dir"
+# Очистка целевой папки
+rm -rf "$target_dir"
+mkdir -p "$target_dir"
 
-    # Упаковка чарта
-    helm package "$dir" --destination "$target_dir"
+# Упаковка чарта
+helm package "$SOURCE_DIR" --destination "$target_dir"
 
-    # Обновление индекса для проекта
-    helm repo index "$target_dir" --url "$BASE_URL/$project_name"
-  fi
-done
+# Создание файла метаданных Artifact Hub в формате YAML
+cat << EOF > "$target_dir/artifacthub-repo.yaml"
+repositoryID: $ARTIFACTHUB_REPO_ID
+owners:
+  - name: bralbral
+    email: https://github.com/bralbral
+EOF
+
+# Создание файла индекса Helm
+helm repo index "$target_dir" --url "$BASE_URL/$project_name"
+
+echo "Artifact Hub repository metadata and index updated successfully for project: $project_name"
